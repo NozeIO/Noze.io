d7456305117cb2a32cb9bca0ff79310fdfdbb771
Helge Hess <devteam@zeezide.de>
2016-06-27 19:17:10 +0200

    Fix dependency.

--- Samples/httpd-cookies/Package.swift
+++ Samples/httpd-cookies/Package.swift
@@ -4,6 +4,6 @@ let package = Package(
   name: "httpd-cookies",
   dependencies: [
     .Package(url: "../..",
-             majorVersion: 0, minor: 0)
+             majorVersion: 0, minor: 2)
   ]
 )
