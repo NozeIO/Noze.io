94201a0d53d81e0a2a4f2387afa876a2416eacf6
Fabian Fett <fabianfett@mac.com>
2016-10-26 19:09:32 +0200

    Removed `func parseAuthorizationValue`. Renamed Credentials.

--- Sources/http/BasicAuth.swift
+++ Sources/http/BasicAuth.swift
@@ -12,7 +12,7 @@ public let basicAuth = BasicAuth.self
 
 public class BasicAuth : NozeModule {
 
-  public struct BasicAuthCredentials {
+  public struct Credentials {
     public let username : String
     public let password : String
   }
@@ -22,17 +22,22 @@ public class BasicAuth : NozeModule {
     case InvalidBasicAuthorizationHeader
   }
 
-  public static func auth(req: IncomingMessage) throws -> BasicAuthCredentials {
+  public static func auth(req: IncomingMessage) throws -> Credentials {
     
     guard let authorization = req.headers[ci: "Authorization"] as? String else {
       throw Error.MissingAuthorizationHeader
     }
     
-    guard let base64 = parseAuthorizationValue(matching : "Basic",
-                                               value    : authorization)
-     else {
+    let parts = authorization
+      .utf8
+      .split(omittingEmptySubsequences: true) { $0 == 32 || $0 == 9 }
+      .map { String.init($0)! }
+    
+    guard parts.count == 2 && "basic" == parts[0].lowercased() else { 
       throw Error.InvalidBasicAuthorizationHeader
     }
+    
+    let base64 = parts[1]
       
     // Split the result at the ':' (ASCII 58). Split only once.
     let split = Base64.decode(string: base64).split(separator: 58, maxSplits: 1)
@@ -47,29 +52,6 @@ public class BasicAuth : NozeModule {
       throw Error.InvalidBasicAuthorizationHeader
     }
     
-    return BasicAuthCredentials(username: username, password: password)
+    return Credentials(username: username, password: password)
   }
-  
-  public static func parseAuthorizationValue(matching schema : String,
-                                             ignoringCase    : Bool = true,
-                                             value           : String)
-                -> String?
-  {
-    let parts = value
-      .utf8
-      .split(omittingEmptySubsequences: true) { $0 == 32 || $0 == 9 }
-      .map { String.init($0)! }
-    
-    guard parts.count == 2 else { return nil }
-    
-    if ignoringCase {
-      guard schema.lowercased() == parts[0].lowercased() else { return nil }
-    }
-    else {
-      guard schema == parts[0] else { return nil }
-    }
-    
-    return parts[1]
-  }
-
 }
