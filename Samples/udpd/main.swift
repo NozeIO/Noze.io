9cf4c9b21757476bfa9676e59bfbe2549e50c278
Helge Hess <devteam@zeezide.de>
2017-03-20 15:05:53 +0100

    Cleanup test tool
    - use console
    - replace newlines in output

--- Samples/udpd/main.swift
+++ Samples/udpd/main.swift
@@ -1,23 +1,31 @@
 // Noze.io UDP server
 // - to compile in Swift 3 invoke: swift build
 // - to run result: .build/debug/udpd
-// - to try it: nc.openbsd -u <interface> 10000
+// - to try it:
+//   - Linux: nc.openbsd -u <interface> 10000
+//   - macOS: nc -vu4 localhost 10000
 
-import Foundation
+import Foundation // for String/Data
 import dgram
-import net
-import xsys
+import console
 
 let sock = dgram.createSocket()
 sock
-  .onListening { address in print("dgram: bound to \(address)") }
-  .onError { err in print("error: \(err)") }
-  .onMessage { (msg, from) in
-    print("received: \(msg) from \(from)")
-    if let decoded = String(data: Data(msg), encoding: .utf8) {
-      print("decoded: \(decoded)")
-      print("calling send on \(sock) with \([UInt8](decoded.uppercased().utf8))")
-      sock.send([UInt8](decoded.uppercased().utf8), to: from)
+  .onListening { address in console.info ("dgram: bound to:", address) }
+  .onError     { err     in console.error("error:", err) }
+  .onMessage   { (msg, from) in
+    console.log("received: \(msg) from \(from)")
+    
+    guard let decoded = String(data: Data(msg), encoding: .utf8) else {
+      console.info("could not decode packet: \(msg)")
+      return
     }
+    
+    console.log("  decoded:",
+                decoded.replacingOccurrences(of: "\n", with: "\\n"))
+    
+    let packet = [UInt8](decoded.uppercased().utf8)
+    console.log("  calling send on \(sock) with:", packet)
+    sock.send(packet, to: from)
   }
   .bind(10000)
