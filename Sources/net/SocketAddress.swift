8982658f8e7694c54ebc365fbb1b77ad4a076943
Helge Heß <devteam@zeezide.de>
2017-09-29 11:39:13 +0200

    Swift 4: .. is more picky about private funcs
    When a private function is used in a default argument,
    it now needs to be as visible on the call site.
    
    Which is kinda lame, but OK.

--- Sources/net/SocketAddress.swift
+++ Sources/net/SocketAddress.swift
@@ -18,7 +18,7 @@ import fs
 // import Darwin.POSIX.netinet.`in` - this doesn't seem to work
 // import struct Darwin.POSIX.netinet.`in`.sockaddr_in - neither
 
-let INADDR_ANY = in_addr(s_addr: 0)
+public let INADDR_ANY = in_addr(s_addr: 0)
 
 /**
  * in_addr represents an IPv4 address in Unix. We extend that a little bit
